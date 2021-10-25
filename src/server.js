e51e62f3b95a3e6f67efc540846b097551e402b1
david-asem <ddy.asem@gmail.com>
2021-10-25 20:10:20 +0000

    initial commit

---
+++ src/server.js
@@ -0,0 +1,9 @@
+require('dotenv').config({ path: './.env'});
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}...`)
+})
